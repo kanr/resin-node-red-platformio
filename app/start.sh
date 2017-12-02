a6a6cd6146dd961f607c4425bc181aaeb144b052
Connor <connor.aitken@gmail.com>
2017-12-01 20:41:07 -0800

    correcting syntax in curl command

--- app/start.sh
+++ app/start.sh
@@ -7,13 +7,13 @@ mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
 # Install PlatformIO
-python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
+#curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
 
 # change directory to folder where is located downloaded "get-platformio.py"
-cd /path/to/dir/where/is/located/get-platformio.py/script
+#cd /path/to/dir/where/is/located/get-platformio.py/script
 
 # run it
-python get-platformio.py
+#python get-platformio.py
 
 # Start app
 node-red --settings /usr/src/app/settings.js

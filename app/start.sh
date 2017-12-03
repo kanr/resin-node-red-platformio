56c4e8cd3a7d7b43269056833dace0ff415fa5bd
Connor <connor.aitken@gmail.com>
2017-12-02 17:30:52 -0800

    adding arduino pio projects to subfolders

--- app/start.sh
+++ app/start.sh
@@ -6,14 +6,5 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
-# Install PlatformIO
-#curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
-
-# change directory to folder where is located downloaded "get-platformio.py"
-#cd /path/to/dir/where/is/located/get-platformio.py/script
-
-# run it
-#python get-platformio.py
-
 # Start app
 node-red --settings /usr/src/app/settings.js

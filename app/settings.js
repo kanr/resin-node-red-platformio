b47a19ae65aafd1bb4257947987636ffb353d156
Connor <connor.aitken@gmail.com>
2018-03-23 00:11:30 -0700

    fixing whitespace for formatting

--- app/settings.js
+++ app/settings.js
@@ -178,7 +178,7 @@ module.exports = {
     functionGlobalContext: {
         process: process,
         os: require('os'),
-        onoff:require('onoff'),
+        onoff: require('onoff'),
         // octalbonescript:require('octalbonescript'),
         // jfive:require("johnny-five"),
         // j5board:require("johnny-five").Board({repl:false})

54e847b08223cd1c0e349238b723641ad417c5dc
Connor <connor.aitken@gmail.com>
2018-08-26 10:55:34 -0700

    adding am2315 sensor deps

---
+++ app/arduinouno/src/README.txt
@@ -0,0 +1,33 @@
+This is a library for the AM2315 Humidity + Temp sensor
+
+Designed specifically to work with the AM2315 in the Adafruit shop 
+  ----> https://www.adafruit.com/products/1293
+
+These sensors use I2C to communicate, 2 pins are required to interface
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada for Adafruit Industries.  
+BSD license, all text above must be included in any redistribution
+
+Check out the links above for our tutorials and wiring diagrams 
+
+To download. click the ZIP button in the top-middle navbar, 
+rename the uncompressed folder Adafruit_AM2315. 
+Check that the Adafruit_AM2315 folder contains Adafruit_AM2315.cpp and Adafruit_AM2315.h
+
+Place the Adafruit_AM2315 library folder your arduinosketchfolder/libraries/ folder. 
+You may need to create the libraries subfolder if its your first library. Restart the IDE.
+
+To connect the AM2315 to the Arduino:
+
+- RED wire goes to +5V
+- BLACK wire goes to Ground
+- WHITE wire goes to to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc that's Analog 5
+- YELLOW wire goes to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc that's Analog 4
+
+Also see http://arduino.cc/en/Reference/Wire
+
+We also have a great tutorial on Arduino library installation at:
+http://learn.adafruit.com/adafruit-all-about-arduino-libraries-install-use
--- app/arduinouno/src/main.cpp
+++ app/arduinouno/src/main.cpp
@@ -1,38 +1,42 @@
-/********************************************************************/
-// First we include the libraries
-#include <OneWire.h>
-#include <DallasTemperature.h>
-/********************************************************************/
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
-/********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices
-// (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
-/********************************************************************/
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
-/********************************************************************/
-void setup(void)
-{
- // start serial port
- Serial.begin(9600);
- Serial.println("Dallas Temperature IC Control Library Demo");
- // Start up the library
- sensors.begin();
+#include <Wire.h>
+#include <Adafruit_AM2315.h>
+
+/*************************************************** 
+  This is an example for the AM2315 Humidity + Temp sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/1293
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect RED of the AM2315 sensor to 5.0V
+// Connect BLACK to Ground
+// Connect WHITE to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect YELLOW to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+
+Adafruit_AM2315 am2315;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("AM2315 Test!");
+
+  if (! am2315.begin()) {
+     Serial.println("Sensor not found, check wiring & pullups!");
+     while (1);
+  }
 }
-void loop(void)
-{
- // call sensors.requestTemperatures() to issue a global temperature
- // request to all devices on the bus
-/********************************************************************/
- Serial.print(" Requesting temperatures...");
- sensors.requestTemperatures(); // Send the command to get temperature readings
- Serial.println("DONE");
-/********************************************************************/
- Serial.print("Temperature is: ");
- Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?
-   // You can have more than one DS18B20 on the same bus.
-   // 0 refers to the first IC on the wire
-   delay(1000);
+
+void loop() {
+  Serial.print("Hum: "); Serial.println(am2315.readHumidity());
+  Serial.print("Temp: "); Serial.println(am2315.readTemperature());
+
+  delay(1000);
 }
